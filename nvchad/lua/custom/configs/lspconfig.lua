2439d33371ac0eaf618aeca8f01de44ecc16ae60
Jezzrel <sjezzrey@gmail.com>
2023-10-04 09:30:52 +0800

    dotfiles commit

--- nvchad/lua/custom/configs/lspconfig.lua
+++ nvchad/lua/custom/configs/lspconfig.lua
@@ -22,6 +22,7 @@ lspconfig.emmet_ls.setup({
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
+-- local servers = { "bashls" "clangd" "cmake" "cssls" "dockerls" "eslint" "emmet_ls" "html" "jsonls" "lua_ls" "intelephense" "pyright" "ruby_ls" "sqlls" "tailwindcss" "tsserver" "volar" "yamlls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
